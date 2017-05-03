bffacd42d21e6c46032a3ce308b4814a206a307d
DELL-5557\Allan Horst <allanhorstwillig@gmail.com>
2017-05-02 22:27:30 -0300

    Second commit

---
+++ app/src/main/java/br/edu/unisep/mymemories/CadastroMedia.java
@@ -0,0 +1,18 @@
+package br.edu.unisep.mymemories;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CadastroMedia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_media);
+    }
+
+    public void abrirCamera(View v) {
+
+    }
+}
